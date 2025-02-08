7d34965e397edee11db5cc0dbe59145e6dd90e58
DubavetsOlga <89518212+DubavetsOlga@users.noreply.github.com>
2025-02-08 09:45:23 +0100

    Revert "Scrum 6"

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import '../src/app/globals.css';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#0D0D0D',
+        },
+      ],
+    },
+  },
+};
+
+export default preview;
