e898a67ca0a85ae528d12a81fd9694a7af2de3d9
Ramil <fathulislamovrr@gmail.com>
2025-02-12 02:18:16 +0500

    fix: add bottom border and add adaptive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,18 +7,23 @@ import { SelectItem } from './SelectBox/SelectItem';
 
 export function Header(): ReactElement {
   return (
-    <header className="text-light-100 mr-auto ml-auto flex h-[60px] w-full max-w-[1280px] items-center justify-between pr-[60px] pl-[60px]">
-      <Link href="/" className="large-text">
-        Inctagram
-      </Link>
-      <SelectBox defaultValue={'value2'}>
-        <SelectItem value={'value1'} icon={ruFlag}>
-          Russian
-        </SelectItem>
-        <SelectItem value={'value2'} icon={ukFlag}>
-          English
-        </SelectItem>
-      </SelectBox>
+    <header className="border-dark-300 border-b-1">
+      <div className="mx-auto flex h-[60px] w-full max-w-[1280px] items-center justify-between px-[60px] max-md:px-[15px]">
+        <Link href="/" className="large-text">
+          Inctagram
+        </Link>
+        <SelectBox
+          defaultValue={'value2'}
+          className="bg-dark-900 border-dark-100 max-md:w-[70px] max-md:border-hidden"
+        >
+          <SelectItem value={'value1'} icon={ruFlag}>
+            <span className="max-md:hidden">Russian</span>
+          </SelectItem>
+          <SelectItem value={'value2'} icon={ukFlag}>
+            <span className="max-md:hidden">English</span>
+          </SelectItem>
+        </SelectBox>
+      </div>
     </header>
   );
 }
