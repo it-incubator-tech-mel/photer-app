aeba4f73c0c22cfaad4a49d105b73a75ef821e47
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2025-02-13 19:46:36 +0300

    'add Sign Up page layout'

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,11 +1,59 @@
 import { Header } from '@/components/header/Header';
 import { ReactElement } from 'react';
+import { Card, IconSprite, Input, Checkbox, Button } from '@/components';
+import Link from 'next/link';
 
 export default function Page(): ReactElement {
   return (
     <>
       <Header />
-      <p>Sign Up</p>
+      <Card className="m-auto mt-6 flex h-162 w-[378px] flex-col items-center">
+        <h1 className="h1-text mt-[23px]">Sign Up</h1>
+        <div className="mt-[13px] flex gap-15">
+          <IconSprite
+            iconName="google"
+            width="36"
+            height="36"
+            className="fill-red-500"
+          />
+          <IconSprite
+            iconName="github"
+            width="36"
+            height="36"
+            className="fill-white"
+          />
+        </div>
+        <div className="m-6 mx-auto">
+          <Input className="w-[330px]" label={'Username'} />
+          <Input className="w-[330px]" label={'Email'} />
+          <Input className="w-[330px]" label={'Password'} />
+          <Input className="w-[330px]" label={'Password confirmation'} />
+          <div className="flex items-center justify-center gap-3">
+            <Checkbox className="" id="terms" />
+            <label htmlFor="terms" className="small-text">
+              I agree to the{' '}
+              <Link
+                href="/terms-of-service"
+                className="text-accent-300 underline"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href="/privacy-policy"
+                className="text-accent-300 underline"
+              >
+                Privacy Policy
+              </Link>
+            </label>
+          </div>
+          <Button className="my-5 w-[330px]">Sign Up</Button>
+          <p className="regular-text-16 text-center">Do you have an account?</p>
+          <Button variant="text" className="mt-1 w-[330px]">
+            Sign In
+          </Button>
+        </div>
+      </Card>
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,6 @@ export { Textarea } from './textarea/Textarea';
 export { Checkbox } from './checkbox/checkbox';
 export { Card } from './card/card';
 export { Header } from './header/Header';
+export { IconSprite } from './icon/IconSprite';
+export { Input } from './input/Input';
+export { Button } from './button/Button';
