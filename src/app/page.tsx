657bd385b4e609eb1a70b1021eeb55a0ddb2c6da
ST4RFKR <nikelodiym1210@gmail.com>
2025-03-21 10:23:12 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { Button } from '@/shared/ui';
+import Link from 'next/link';
 import { ReactElement } from 'react';
 
 export default function Home(): ReactElement {
   return (
     <div>
-      <main></main>
+      <main>
+        Вы не вошли в систему или ваша сессия истекла, авторизуйтесь пожалуйста{' '}
+        <Button asChild variant={'text'}>
+          <Link href="/sign-in">Войти</Link>
+        </Button>
+      </main>
     </div>
   );
 }
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useGetMeQuery } from '@/features/auth/api/authApi';
 import { useLogout } from '@/features/auth/hooks/useLogout';
 import { LogoutModal } from '@/features/auth/ui/login-form/LogoutForm';
 import { LogoutButton } from '@/widgets/logout-button/LogoutButton';
@@ -7,9 +8,11 @@ import { ReactElement } from 'react';
 
 export default function Page(): ReactElement {
   const { isOpen, openModal, closeModal, confirmLogout } = useLogout();
+  const { data } = useGetMeQuery();
 
   return (
     <div>
+      <h1>Profile {data?.email}</h1>
       <LogoutButton openModal={openModal} />
       <LogoutModal
         open={isOpen}
