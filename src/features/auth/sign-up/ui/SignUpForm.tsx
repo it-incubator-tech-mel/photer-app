41e7aa51f3a8a396f7148f0a2a8100768e9a0ab8
Evg_S <nikelodiym1210@gmail.com>
2025-03-31 15:22:31 +0300

    fix layout

--- src/features/auth/sign-up/ui/SignUpForm.tsx
+++ src/features/auth/sign-up/ui/SignUpForm.tsx
@@ -15,7 +15,7 @@ export default function SignUpForm(): React.ReactElement {
     <Card className="m-0-auto mt-6 flex min-h-162 w-[378px] flex-col items-center justify-center">
       <h1 className="h1-text mt-[23px]">Sign Up</h1>
       <OAuthLinks />
-      <form onSubmit={onFormSubmit} className="w-full px-6">
+      <form onSubmit={onFormSubmit} className="mt-6 w-full px-6">
         <FormFields
           register={formState.register}
           errors={formState.errors}
