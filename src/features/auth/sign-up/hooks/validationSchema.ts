3cc8f04a88d856ed2f5aec4e2afeab2d12158184
Aleksey Astashonok <lextapir191919@gmail.com>
2025-08-30 01:27:59 +0300

    fix: password validation

--- src/features/auth/sign-up/hooks/validationSchema.ts
+++ src/features/auth/sign-up/hooks/validationSchema.ts
@@ -18,8 +18,8 @@ export const signUpSchema = z
       .min(6, 'Password must be at least 6 characters long')
       .max(30, 'Maximum number of characters 30')
       .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,30}$/,
-        'Password must contain at least one lowercase letter, one uppercase letter, one digit.'
+        /^[a-zA-Z0-9_-]+$/,
+        'Password can only contain letters, numbers, underscores, and hyphens'
       ),
     terms: z.boolean().refine((val) => val, {
       message: 'You must accept the terms',
